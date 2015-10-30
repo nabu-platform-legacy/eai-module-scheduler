9a1c6b09eec7fc91102b6746d43d3858d2d37944
alex <alex@nabu.be>
2015-10-30 13:51:12 +0100

    Added enumerations Pom management

--- src/main/java/be/nabu/eai/artifacts/scheduler/complex/ComplexSchedulerConfiguration.java
+++ src/main/java/be/nabu/eai/artifacts/scheduler/complex/ComplexSchedulerConfiguration.java
@@ -1,10 +1,13 @@
 package be.nabu.eai.artifacts.scheduler.complex;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import be.nabu.eai.api.Enumerator;
+import be.nabu.eai.api.ValueEnumerator;
 import be.nabu.eai.artifacts.scheduler.base.BaseSchedulerConfiguration;
 
 @XmlRootElement(name = "complexScheduler")
@@ -63,34 +66,82 @@ public class ComplexSchedulerConfiguration extends BaseSchedulerConfiguration {
 	public void setMonthOfYear(List<MonthOfYear> monthOfYear) {
 		this.monthOfYear = monthOfYear;
 	}
+	@ValueEnumerator(enumerator = YearEnumerator.class)
 	public List<Integer> getYear() {
 		return year;
 	}
 	public void setYear(List<Integer> year) {
 		this.year = year;
 	}
+	@ValueEnumerator(enumerator = DayEnumerator.class)
 	public List<Integer> getDayOfMonth() {
 		return dayOfMonth;
 	}
 	public void setDayOfMonth(List<Integer> dayOfMonth) {
 		this.dayOfMonth = dayOfMonth;
 	}
+	@ValueEnumerator(enumerator = HourEnumerator.class)
 	public List<Integer> getHour() {
 		return hour;
 	}
 	public void setHour(List<Integer> hour) {
 		this.hour = hour;
 	}
+	@ValueEnumerator(enumerator = MinuteEnumerator.class)
 	public List<Integer> getMinute() {
 		return minute;
 	}
 	public void setMinute(List<Integer> minute) {
 		this.minute = minute;
 	}
+	@ValueEnumerator(enumerator = MinuteEnumerator.class)
 	public List<Integer> getSecond() {
 		return second;
 	}
 	public void setSecond(List<Integer> second) {
 		this.second = second;
 	}
+	
+	public static class DayEnumerator implements Enumerator {
+		@Override
+		public List<?> enumerate() {
+			List<Integer> values = new ArrayList<Integer>();
+			for (int i = 1; i <= 31; i++) {
+				values.add(i);
+			}
+			return values;
+		}
+	}
+	public static class HourEnumerator implements Enumerator {
+		@Override
+		public List<?> enumerate() {
+			List<Integer> values = new ArrayList<Integer>();
+			for (int i = 0; i <= 23; i++) {
+				values.add(i);
+			}
+			return values;
+		}
+	}
+	public static class MinuteEnumerator implements Enumerator {
+		@Override
+		public List<?> enumerate() {
+			List<Integer> values = new ArrayList<Integer>();
+			for (int i = 0; i <= 59; i++) {
+				values.add(i);
+			}
+			return values;
+		}
+	}
+	public static class YearEnumerator implements Enumerator {
+		@Override
+		public List<?> enumerate() {
+			List<Integer> values = new ArrayList<Integer>();
+			Calendar calendar = Calendar.getInstance();
+			for (int i = calendar.get(Calendar.YEAR); i <= 50; i++) {
+				values.add(i);
+			}
+			return values;
+		}
+	}
+	
 }
